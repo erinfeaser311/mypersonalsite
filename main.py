36136b748c738d353203ab67fd0aad0f6146e32a
Erin Feaser <erinfeaser311@gmail.com>
2021-02-24 00:29:50 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/courses")
+def courses():
+    return render_template("courses.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/goals")
+def goals():
+    return render_template("goals.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
