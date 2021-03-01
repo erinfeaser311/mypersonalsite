ce2a600019b7fa2192db9911b0842827bfb692d9
Erin Feaser <erinfeaser311@gmail.com>
2021-03-01 15:47:45 -0500

    Added resume

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -20,6 +20,11 @@ def projects():
 def goals():
     return render_template("goals.html")
 
+@app.route('/download_resume')
+def download_resume():
+    return send_from_directory("static/files/",
+                               "erin_feaser_resume.pdf", as_attachment=True)
+
 if __name__=="__main__":
-    #app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    #app.run(debug=True)
